8fb4fe1d6c64623d785e2dafdad5fa0345ad6925
toppatayouhei-debug <toppata.youhei@mail.ibk.ed.jp>
2026-07-22 10:27:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def clean_text(t):
 # 3. メイン画面
 # ==================================================
 st.markdown('<div class="main-title">🚀 Pass Pack -Type A-</div>', unsafe_allow_html=True)
-st.markdown('<div class="sub-title">英語・地歴・公民・理科基礎 統合学習ツール</div>', unsafe_allow_html=True)
+st.markdown('<div class="sub-title">文系受験生のための統合型学習ツール</div>', unsafe_allow_html=True)
 
 subject = st.selectbox("学習する科目を選択", [
     "選択してください", "共通テスト現代文（漢字）", "システム英単語", "暗唱例文集", "頻出！英文法入試問題",
